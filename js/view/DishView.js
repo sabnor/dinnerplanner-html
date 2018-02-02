726a60cc29fa927a280871d155b018c77612210c
Karolin Valaszkai <karolinv@kth.se>
2018-02-02 14:36:27 +0100

    k

--- js/view/DishView.js
+++ js/view/DishView.js
@@ -1,47 +1,58 @@
+//@param {jQuery object} container - references the HTML parent element that contains the view.
+  //@param {Object} model - the reference to the Dinner Model
 
- *
- * @param {jQuery object} container - references the HTML parent element that contains the view.
- * @param {Object} model - the reference to the Dinner Model
- */
-var DishView = function (container, model) {
+  var DishView = function (container, model) {
+    console.log("test1")
+loadDishes();
 
-	/**
-	 * We use the @method find() on @var {jQuery object} container to look for various elements
-	 * inside the view in orther to use them later on. For instance:
-	 *
-	 * @var {jQuery object} numberOfGuests is a reference to the <span> element that
-	 * represents the placeholder for where we want to show the number of guests.
-	 */
-var numberOfGuests = container.find("#dishViewCard");
+	// This function loads the dishes for a given type
+	function loadDishes() {
+    console.log("test2")
+
+		// Gather data, find the dish list div and empty it
+		var dishData = model.getAllDishes();
+    console.log(dishData)
+
+		var dishList = container.find("#dishList");
+		dishList.empty();
+
+// Create all the cards for dishes
+		dishData.forEach(function (dish) {
+
+			var div = makeDishView(dish);
+			div.appendTo(dishList);
+		});
+	}
 
 function makeDishView(dish) {
+  console.log("test4")
+
   //Outer div
 	var divElement = document.createElement('div')
 	var divObject = $(divElement).addClass('col-xs-3');
 
   //Card
-	var card = document.createElement('div');
-	$(card).addClass('pictureview centerr')
+	var box = document.createElement('div');
+	$(box).addClass('pictureview centerr')
 			.appendTo(divObject);
 
   var dishImg = document.createElement('img');
   $(dishImg).addClass('tumnagel xs-1').attr('src','images/'+dish.image)
-			.appendTo(divObject);
+			.appendTo(box);
 
-	<div class="row">
-    <div class="pictureview centerr">
+  var label = document.createElement('label');
+  $(label).text(dish.name)
+  .appendTo(box)
+console.log("test5")
 
-    <a href="#">
-         <img src="images/bakedbrie.jpg" alt="" class="tumnagel mb-1">
-    </a>
-    <label id="FoodView">Baked brie</label>
-
-  </div>
+    return divObject;
+  }
+  console.log("test6")
 
 }
 
-	numberOfGuests.html("<h1>"+model.getSelectedDish('starter')+"</h1>");
-	console.log("Wazzap");
+	//numberOfGuests.html("<h1>"+model.getSelectedDish('starter')+"</h1>");
+	//console.log("Wazzap");
 
 	/**
 	 * When we want references to some view elements to be available from outside of view, we
@@ -53,11 +64,9 @@ function makeDishView(dish) {
 	 * this button and do something with it (see Lab 2).
 	 *
 	 */
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
 
-	/**
-	 * Here we use @var {jQuery object} numberOfGuests that is a reference to <span>
-	 * in our view to dynamically set it's value to "Hello World".
-	 */
-}
+	//this.plusButton = container.find("#plusGuest");
+	//this.minusButton = container.find("#minusGuest");
+
+	// @var {jQuery object} numberOfGuests that is a reference to <span>
+	 // in our view to dynamically set it's value to "Hello World".
