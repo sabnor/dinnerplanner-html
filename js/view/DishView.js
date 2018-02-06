79bbe7d45e3aecaf61f3647e4ddfc48552e5242a
Sabina Nordell <sabinanordell@n143-p92.eduroam.kth.se>
2018-02-06 11:59:44 +0100

    ingredients

--- js/view/DishView.js
+++ js/view/DishView.js
@@ -6,6 +6,9 @@
 
     var dishObject = model.getDish(100)
 
+
+    //--------------------------------------------------------------------
+    //Creating the dish-information
     var labelElement = document.createElement('label')
     var labelObject = $(labelElement).text(dishObject.name);
 
@@ -20,12 +23,42 @@
     $(descriptionElement).text(dishObject.description)
     .appendTo(divElement);
 
-
-
-
     var dishLabel = container.find("#dishLabel");
     dishLabel.html(labelElement);
 
+    var numberOfGuests = container.find("#numberOfGuests");
+    numberOfGuests.html(model.getNumberOfGuests());
+
+//--------------------------------------------------------------------
+//Creating the ingredient-rows
+
+
+var outerDiv = document.createElement('div')
+
+dishObject.ingredients.forEach(function(ingredientObject){
+    var price = ingredientObject['price']
+
+    var rowElement = document.createElement('div')
+    $(rowElement).addClass('row');
+
+    var ingredientQuantityElement = document.createElement('div');
+    $(ingredientQuantityElement).addClass('col').text(ingredientObject['quantity']+' '+ingredientObject['unit'])
+    .appendTo(rowElement);
+
+    var ingredientNameElement = document.createElement('div');
+    $(ingredientNameElement).addClass('col').text(ingredientObject['name'])
+    .appendTo(rowElement);
+
+    var ingredientPriceElement = document.createElement('div');
+    $(ingredientPriceElement).addClass('col').text(ingredientObject['price']+model.getCurrency())
+    .appendTo(rowElement);
+
+    $(rowElement).appendTo(outerDiv)
+});
+console.log(outerDiv)
+
+var ingredientrows = container.find("#ingredientrows");
+ingredientrows.html(outerDiv);
 
 
 
