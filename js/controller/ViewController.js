060a8f53e2d158089dab855a47fb95957f89a680
Karolin Valaszkai <karolinv@kth.se>
2018-02-14 10:43:23 +0100

    k

--- js/controller/ViewController.js
+++ js/controller/ViewController.js
@@ -1,9 +1,5 @@
 //ExampleViewController Object constructor
-<<<<<<< HEAD
-var ViewController = function(view, model ) {
-=======
 var ViewController = function(view, model, generalStateController) {
->>>>>>> 5be1c989aed07f9a27622e66550371b2b9cbcafd
 
  view.plusButton.click(function(){
    console.log("plus")
